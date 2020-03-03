926368191d5e79ca5a85db58ba7022d21c09981a
Jin <jbjxu@thoughtworks.com>
2020-03-03 17:41:56 +0800

    feat: use Player class instead of playerName

--- src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
@@ -2,36 +2,33 @@ package cn.xpbootcamp.tennis;
 
 public class TennisGame3 implements TennisGame {
 
-    private int p2;
-    private int p1;
-    private String p1Name;
-    private String p2Name;
+    private Player player1;
+    private Player player2;
 
     public TennisGame3(String p1Name, String p2Name) {
-        this.p1Name = p1Name;
-        this.p2Name = p2Name;
+        this.player1 = new Player(p1Name, 0);
+        this.player2 = new Player(p2Name, 0);
     }
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+        if (player1.getPoint() < 4 && player2.getPoint() < 4 && !(player1.getPoint() + player2.getPoint() == 6)) {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+            s = p[player1.getPoint()];
+            return (player1.getPoint() == player2.getPoint()) ? s + "-All" : s + "-" + p[player2.getPoint()];
         } else {
-            if (p1 == p2)
+            if (player1.getPoint() == player2.getPoint())
                 return "Deuce";
-            s = p1 > p2 ? p1Name : p2Name;
-            return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+            s = player1.getPoint() > player2.getPoint() ? player1.getName() : player2.getName();
+            return ((player1.getPoint() - player2.getPoint()) * (player1.getPoint() - player2.getPoint()) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
 
     public void wonPoint(String playerName) {
-        if (this.p1Name.equals(playerName))
-            this.p1 += 1;
+        if (player1.getName().equals(playerName))
+            player1.wonPoint();
         else
-            this.p2 += 1;
-
+            player2.wonPoint();
     }
 
 }
